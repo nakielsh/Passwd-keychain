546203a3fa0d780cbbafe4f6f86f84ab0de0266e
nakielsh <hubert.nakielski@gmail.com>
2021-12-03 17:53:39 +0100

    added api login, creating access and refresh tokens, and saving data to database (passwords are encrypted)

--- passwd-keychain/src/main/java/pw/edu/pl/passwdkeychain/PasswdKeychainApplication.java
+++ passwd-keychain/src/main/java/pw/edu/pl/passwdkeychain/PasswdKeychainApplication.java
@@ -1,13 +1,42 @@
 package pw.edu.pl.passwdkeychain;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pw.edu.pl.passwdkeychain.domain.AppUser;
+import pw.edu.pl.passwdkeychain.domain.Password;
+import pw.edu.pl.passwdkeychain.service.AppUserService;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PasswdKeychainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PasswdKeychainApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(AppUserService appUserService) {
+		return args -> {
+			Password pass1 = appUserService.savePassword(new Password( null, "Faceboook", "test"));
+			Password pass2 = appUserService.savePassword(new Password( null, "Twitter", "twotterPasswd"));
+			Password pass3 = appUserService.savePassword(new Password( null, "Instagram", "instapass"));
+
+			appUserService.saveAppUser(new AppUser(null, "Lisek Ogrodniczek", "lisogr", "hasloliska", "pasterpasslika", new ArrayList<>()));
+
+			appUserService.addPasswordToAppUser("lisogr", pass1.getId());
+			appUserService.addPasswordToAppUser("lisogr", pass2.getId());
+			appUserService.addPasswordToAppUser("lisogr", pass3.getId());
+		};
 	}
 
 }
--- passwd-keychain/src/main/java/pw/edu/pl/passwdkeychain/service/AppUserServiceImpl.java
+++ passwd-keychain/src/main/java/pw/edu/pl/passwdkeychain/service/AppUserServiceImpl.java
@@ -2,6 +2,11 @@ package pw.edu.pl.passwdkeychain.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pw.edu.pl.passwdkeychain.domain.AppUser;
 import pw.edu.pl.passwdkeychain.domain.Password;
@@ -9,19 +14,24 @@ import pw.edu.pl.passwdkeychain.repo.AppUserRepo;
 import pw.edu.pl.passwdkeychain.repo.PasswordRepo;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
     private final AppUserRepo appUserRepo;
     private final PasswordRepo passwordRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public AppUser saveAppUser(AppUser appUser) {
         log.info("Saving new AppUser: {} to the database", appUser.getName());
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+        appUser.setMasterPassword(passwordEncoder.encode(appUser.getMasterPassword()));
         return appUserRepo.save(appUser);
     }
 
@@ -49,5 +59,19 @@ public class AppUserServiceImpl implements AppUserService {
     public List<AppUser> getAppUsers() {
         log.info("Fetching all users");
         return appUserRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepo.findAppUserByUsername(username);
+        if(appUser == null) {
+            log.info("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("User"));
+        return new org.springframework.security.core.userdetails.User(appUser.getUsername(), appUser.getPassword(), authorities);
     }
 }
